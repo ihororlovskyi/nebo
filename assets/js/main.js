54d48db45273566c46cd78cb7b7c81a62a1eb6aa
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-03-24 03:17:20 +0200

    add remodal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,4 @@
 /**
-* jQuery-One-Page-Nav
-* https://github.com/davist11/jQuery-One-Page-Nav
-*/
-$(document).ready(function() {
-    $('.js-page-nav').onePageNav({
-        currentClass: 'mainmenu__li--selected',
-        changeHash: true,
-        scrollSpeed: 1100,
-        scrollThreshold: 0.011,
-        easing: 'easeInOutExpo',
-    });
-});
-
-/**
 * totop
 */
 $(function() {
@@ -28,21 +14,39 @@ $(function() {
     });
 });
 
-/**
-* slick
-* https://github.com/kenwheeler/slick
-*/
 $(document).ready(function() {
-    $('#fullpage').fullpage();
-});
+    /**
+    * jQuery-One-Page-Nav
+    * https://github.com/davist11/jQuery-One-Page-Nav
+    */
+    $('.js-page-nav').onePageNav({
+        currentClass: 'mainmenu__li--selected',
+        changeHash: true,
+        scrollSpeed: 1100,
+        scrollThreshold: 0.011,
+        easing: 'easeInOutExpo',
+    });
 
-/**
-* slick
-* https://github.com/kenwheeler/slick
-*/
-$(document).ready(function() {
+    /**
+    * slick
+    * https://github.com/kenwheeler/slick
+    */
+    $('#fullpage').fullpage();
+
+    /**
+    * slick
+    * https://github.com/kenwheeler/slick
+    */
     $('.your-class').slick({
         dots: true
+    });
+
+    /**
+    * Remodal
+    * https://github.com/VodkaBears/Remodal
+    */
+    $('.js-remodal').remodal({
+        // closeOnEscape: false
     });
 });
 
