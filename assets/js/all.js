be757ec81a4b1f7254c159ced50c7057c3c974cf
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-05-29 23:59:13 +0300

    event tag clickable now

--- assets/js/all.js
+++ assets/js/all.js
@@ -391,28 +391,28 @@ function initMap() {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-    // GLOBAL VARIABLES
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
-    var isMobile = {
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i);
-        },
-        any: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-    };
+// GLOBAL VARIABLES
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
 
 /**
 * isotope with imagesloaded
